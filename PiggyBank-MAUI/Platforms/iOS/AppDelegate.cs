9bc8828029b0499cfafafb94043ca1b6af8cecc0
Crisgn12 <cristopher.gonzalez.nunez@est.una.ac.cr>
2025-05-12 00:31:25 -0600

    Creacion del proyecto

---
+++ PiggyBank-MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PiggyBank_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
