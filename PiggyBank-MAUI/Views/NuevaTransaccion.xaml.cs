18bb35b5b5040076a5e5e7ff7426973949918cd2
Crisgn12 <cristopher.gonzalez.nunez@est.una.ac.cr>
2025-05-30 12:58:55 -0600

    Página Nueva Transacción completa

--- PiggyBank-MAUI/Views/NuevaTransaccion.xaml.cs
+++ PiggyBank-MAUI/Views/NuevaTransaccion.xaml.cs
@@ -5,5 +5,18 @@ public partial class NuevaTransaccion : ContentPage
 	public NuevaTransaccion()
 	{
 		InitializeComponent();
-	}
+
+        TipoTransaccionPicker.ItemsSource = new List<string>
+        {
+            "Ingreso",
+            "Gasto"
+        };
+
+        CategoriaPicker.ItemsSource = new List<string>
+        {
+            "Categoria 1",
+            "Categoria 2",
+            "Categoria 3"
+        };
+    }
 }
